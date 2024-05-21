04f32d4e3b259c74eec2abb89c26c9efd69777c1
Nahuel <nahuelg.1992@gmail.com>
2024-05-21 15:06:53 -0300

    Agregando articulos

---
+++ src/dao/handleArticles.js
@@ -0,0 +1,10 @@
+import articleModel from './models/modelArticles.js';
+import FuncionesComunes from './handleCommonFunctions.js';
+import DataBase from './handleDataBase.js';
+
+const dataBase = new DataBase();
+
+class Articles {
+
+}
+export default Articles;
---
+++ src/dao/models/modelArticles.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+
+const collection = 'articletypes';
+
+const medidaSchema = new mongoose.Schema({
+    alto: Number,
+    ancho: Number,
+    profundidad: Number
+})
+
+const articleSchema = new mongoose.Schema({
+    active: Boolean,
+    id: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    nombre: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    descripcion: String,
+    medidas: {
+        type: medidaSchema,
+    },
+    creationData:{
+        type: {
+            date: String,
+            responsible: String,
+        }
+    },
+    modificationData:{
+        type: {
+            date: String,
+            responsible: String,
+        }
+    },
+    deleteData:{
+        type: {
+            date: String,
+            responsible: String,
+        }
+    }
+    }, 
+    //Control de versiones de documento, agrega un campo extra al documento.
+    { versionKey: false }
+);
+
+const articleModel = mongoose.model(collection, articleSchema);
+
+export default articleModel;
